cf658e014308ae9aa34dff0e6d29659efe873cdc
ItzPikaExploits <38090954+ItzPikaExploits@users.noreply.github.com>
2019-09-11 16:56:17 -0700

    Add files via upload

--- commands/moderation/purge.js
+++ commands/moderation/purge.js
@@ -9,9 +9,11 @@ module.exports = {
         if (message.channel.type == "dm") return;
         if (!message.member.hasPermission("MANAGE_MESSAGES")) 
             return message.reply("you do not have the MANAGE_MESSAGES permission!").then(m => m.delete(5000));
-        if (!args[0]) message.reply("how much do you want us to delete?").then(m => m.delete(5000));
+        if (!args[0]) 
+            return message.reply("how much do you want us to delete?").then(m => m.delete(5000));
         let msgAmount = Number(args[0]);
-        if (msgAmount === NaN) message.reply("that is not a valid number!").then(m => m.delete(5000));
+        if (msgAmount === NaN) 
+            return message.reply("that is not a valid number!").then(m => m.delete(5000));
         msgAmount = Math.round(msgAmount);
         msgAmount.channel.bulkDelete(msgAmount)
             .catch(err => console.log(err));
