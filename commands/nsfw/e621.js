2a75a1fc1afbcaf7381894503075e7d4003407ac
ItzPikaExploits <38090954+ItzPikaExploits@users.noreply.github.com>
2019-09-11 18:27:28 -0700

    Add files via upload

--- commands/nsfw/e621.js
+++ commands/nsfw/e621.js
@@ -1,5 +1,6 @@
 const xml2js = require('xml2js');
 const https = require("https");
+const request = require("request");
 module.exports = {
     name: "e621",
     aliases: ["e6"],
@@ -8,42 +9,54 @@ module.exports = {
     usage: "<tags>",
     run: async (client, message, args) => {
         if (message.deletable) message.delete();
-        editedmessage = args.slice(0).join(" ")
-        var url = 'https://e621.net/post/index.json?tags=order:random+' + editedmessage;
-        https.get(url, function(res){
-            var body = '';
-            res.on('data', function(chunk){
-                body += chunk;
-            });
-   
-            res.on('end', function(){
-                var parser = new xml2js.Parser();
-                parser.parseString(body, function (err, result) {
-                    var postCount = result.posts.$.count - 1;
-                    if(postCount > 100) {
-                        postCount = 100;
-                    }
-                    if(postCount > 0) {
-                        var picNum = Math.floor(Math.random() * postCount) + 0;
-                        var e6Pic = result.posts.post[picNum].$.file_url;
-                        let embed = new RichEmbed()
-                            .setColor("LUMINOUS_VIVID_PINK")
-                            .setTitle("E621: " + editedmessage)
-                            .setFooter(e6Pic)
-                            .setImage(e6Pic)
-                            .setDescription("Enjoy~!")
-                            .setTimestamp()
-                        message.channel.send(embed)
-                   
-                    } else {
-                        console.log("Nothing found:", editedmessage);
-                        message.channel.send(":regional_indicator_x: Nobody here but us chickens!");
-                    }
+        params = args.slice(0).join(" ")
+        var tagesto = "";
+        var tagestosplit = params.split(",");
+        for (var i = 0; i < tagestosplit.length; i++) {
+            tagestosplit[i] = tagestosplit[i].trim();
+            tagestosplit[i] = tagestosplit[i].replace(/\s/g, "_");
+        }
 
-                    });
-                });
-            }).on('error', function(e){
-                console.log("Got an error: ", e);
+        tagesto = tagestosplit.join("+");
+
+        if (msg.channel.type === "dm" || msg.channel.name.indexOf("the_art_gallery") != -1 || msg.channel.name.indexOf("furry") != -1 || msg.channel.name.indexOf("2am") != -1 || msg.channel.name.indexOf("nsfw") != -1) {
+            console.log("Safe to post NSFW content.");
+        }
+        else {
+            tagesto += "+rating:safe";
+            if ((tagesto.indexOf("rating:explicit") != -1) || (tagesto.indexOf("penis") != -1) || (tagesto.indexOf("pussy") != -1) || (tagesto.indexOf("anus") != -1) || (tagesto.indexOf("dick") != -1) || tagesto.indexOf("rating:questionable") != -1 || tagesto.indexOf("genitalia") != -1 || tagesto.indexOf("genitals") != -1 || tagesto.indexOf("genital") != -1 || tagesto.indexOf("vagina") != -1 || tagesto.indexOf("cunt") != -1 || tagesto.indexOf("vaginal") != -1 || tagesto.indexOf("vaginal_penetration") != -1 || tagesto.indexOf("sex") != -1 || tagesto.indexOf("fuck") != -1 || tagesto.indexOf("intercourse") != -1 || tagesto.indexOf("cock") != -1) {
+                msg.channel.send("That content isn't appropiate for this channel. Go be naughty elsewhere.", {files:[{attachment: __dirname + bruh.jpg}]});
+                return;
+            }
+        }
+        var estoHeader = {
+            url: 'https://e621.net/post/index.json?tags=order:random+' + tagesto,
+            headers: {
+                'User-Agent': 'Yoshi-Bot/${process.version} (by NeoNinetales on e621)'
+            }
+        }
+
+        request(estoHeader,
+        function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+                var estoThing = JSON.parse(body);
+                if (typeof (estoThing[0]) != "undefined") {
+                    let embed = new RichEmbed()
+                        .setColor("LUMINOUS_VIVID_PINK")
+                        .setTitle("E621: " + editedmessage)
+                        .setFooter(estoThing[0].file_url.toString())
+                        .setImage("https://e621.net/post/show/" + estoThing[0].id.toString())
+                        .setDescription("Enjoy~!")
+                        .setTimestamp()
+                    message.channel.send(embed)
+                }
+                else {
+                    msg.channel.send("No images found. Try different tags.")
+                }
+            }
+            else {
+                console.log(error);
+            }
         });
     }
 };
