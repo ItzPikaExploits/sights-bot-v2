8e9ffcf102028f408d25762328b00af661336333
ItzPikaExploits <38090954+ItzPikaExploits@users.noreply.github.com>
2019-09-18 15:17:51 -0700

    Add files via upload

---
+++ commands/verification/verify.js
@@ -0,0 +1,14 @@
+module.exports = {
+    name: "verify",
+    //aliases: ["bc", "broadcast"],
+    category: "verification",
+    description: "Verifies the message sender.",
+    //usage: "<input>",
+    run: async (client, message, args) => {
+        let vRole = message.guild.roles.get("612350237079633931");
+        message.member.addRole(vRole).catch(err => {
+            console.log(err);
+            return message.channel.send(`${message.author.username}, an error has appeared while trying to verify you: **${err.Message}**.`)
+        });
+    }
+};
